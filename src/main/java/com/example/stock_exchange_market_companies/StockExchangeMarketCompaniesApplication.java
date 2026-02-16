7763d64be4efdc968d736b429e847c081f09e888
Irena Daneva <de.luxe9997@gmail.com>
2026-02-16 23:16:24 +0200

    first commit

---
+++ src/main/java/com/example/stock_exchange_market_companies/StockExchangeMarketCompaniesApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_exchange_market_companies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangeMarketCompaniesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockExchangeMarketCompaniesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stock_exchange_market_companies/StockExchangeMarketCompaniesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_exchange_market_companies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangeMarketCompaniesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
