5e09316d4c0b6d91099908897e6aff319cef9daf
Irena Daneva <de.luxe9997@gmail.com>
2026-02-16 23:35:49 +0200

    chore: initial project skeleton

--- src/main/java/com/example/stockcompanies/StockCompaniesApplication.java
+++ src/main/java/com/example/stockcompanies/StockCompaniesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockExchangeMarketCompaniesApplication {
+public class StockCompaniesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockExchangeMarketCompaniesApplication.class, args);
