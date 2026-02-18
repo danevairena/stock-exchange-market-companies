0a03fecaa7c3810652c4befaad43db779741ade7
Irena Daneva <de.luxe9997@gmail.com>
2026-02-18 10:22:13 +0200

    fix: use test profile for Continuous Integration tests (Github Actions)

--- src/test/java/com/example/stock_exchange_market_companies/StockExchangeMarketCompaniesApplicationTests.java
+++ src/test/java/com/example/stock_exchange_market_companies/StockExchangeMarketCompaniesApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.stock_exchange_market_companies;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+// @ActiveProfiles - use /src/test/resources/application-test.properties
+// instead of /src/main/resources/application.properties
+@ActiveProfiles("test")
 class StockExchangeMarketCompaniesApplicationTests {
 
 	@Test
