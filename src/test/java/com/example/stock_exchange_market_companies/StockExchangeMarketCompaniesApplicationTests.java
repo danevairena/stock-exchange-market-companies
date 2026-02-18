5f3be3d6188404dac97969bc8ecdc5a5b44128ca
Irena Daneva <de.luxe9997@gmail.com>
2026-02-18 10:48:46 +0200

    fix: use correct Spring Boot main class in tests

--- src/test/java/com/example/stock_exchange_market_companies/StockExchangeMarketCompaniesApplicationTests.java
+++ src/test/java/com/example/stock_exchange_market_companies/StockExchangeMarketCompaniesApplicationTests.java
@@ -1,10 +1,12 @@
 package com.example.stock_exchange_market_companies;
 
+import com.example.stockcompanies.StockCompaniesApplication;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = StockCompaniesApplication.class)
 // @ActiveProfiles - use /src/test/resources/application-test.properties
 // instead of /src/main/resources/application.properties
 @ActiveProfiles("test")
